96f0f6fae696d5bf5a664f391c212124e8defd24
koechslin <killian.oechslin@insa-lyon.fr>
2022-04-04 17:29:40 -0400

    Rotate arrow text toward player

--- Assets/Scripts/UIRotateTowardPlayer.cs
+++ Assets/Scripts/UIRotateTowardPlayer.cs
@@ -7,6 +7,6 @@ public class UIRotateTowardPlayer : MonoBehaviour
 
     private void Update()
     {
-        transform.rotation = Quaternion.LookRotation(playerTransform.position - transform.position, new Vector3(0.0f, 1.0f, 0.0f));
+        transform.rotation = Quaternion.LookRotation(transform.position - playerTransform.position, new Vector3(0.0f, 1.0f, 0.0f));
     }
 }
